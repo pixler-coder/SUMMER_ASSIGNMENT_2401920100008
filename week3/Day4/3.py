bbf66560c4833f8755c565a01a0b1a7f829f541a
Aaryan <score.aaryan007@gmail.com>
2026-06-18 12:22:27 +0530

    fix the error

--- week3/Day4/3.py
+++ week3/Day4/3.py
@@ -2,18 +2,30 @@ from typing import List
 
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        max_area = 0
         n = len(heights)
-
+        left_less = [-1] * n
+        right_less = [n] * n
+        
+        stack = []
         for i in range(n):
-            min_height = heights[i]
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            if stack:
+                left_less[i] = stack[-1]
+            stack.append(i)
 
-            for j in range(i, n):
-                min_height = min(min_height, heights[j])
+        stack = []
+        for i in range(n - 1, -1, -1):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            if stack:
+                right_less[i] = stack[-1]
+            stack.append(i)
 
-                width = j - i + 1
-                current_area = min_height * width
-
-                max_area = max(max_area, current_area)
-
+        max_area = 0
+        for i in range(n):
+            width = right_less[i] - left_less[i] - 1
+            current_area = heights[i] * width
+            max_area = max(max_area, current_area)
+            
         return max_area
